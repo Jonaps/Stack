37daf5891bbc77769ff960031fbb0ae265035ca9
Jonaps <105373463+Jonaps@users.noreply.github.com>
2022-06-06 18:06:38 -0400

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,15 @@
+package com.mycompany.stackexample;
+
+public interface Stack<E> {
+   
+    int size();
+    
+    boolean isEmpty();
+    
+    void push(E e);
+    
+    E top();
+    
+    E pop();
+ 
+}
